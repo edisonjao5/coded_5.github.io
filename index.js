f7fe4ae26e18a988f9cf8eefaf0da7b1727e1274
Edison Jao <56049477+edisonjao5@users.noreply.github.com>
2022-01-04 23:02:07 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Change the text between the quotation marks and run your code to see the name of your favorite language animated!
+drawText('Hello and Welcome to <coded5>');
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
