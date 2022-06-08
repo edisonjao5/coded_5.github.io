90f2319ee8fe2a7e04e97d9a1ee64560697b088e
Edison Jao <edisonjao5@icloud.com>
2022-06-08 17:44:14 -0400

    Changing the name to SSilva

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Change the text between the quotation marks and run your code to see the name of your favorite language animated!
-drawText("Welcome to <coded5>");
+drawText("SSilva Inmobiliaria");
 
 // The code below will allow you to interact with the letters
 bounceBubbles();
